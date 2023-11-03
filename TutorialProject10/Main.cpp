7c3bbcf3bba09b056a1ef2055a97cb674e8f9502
Joshua Tomasso <thzombiethatllives@gmail.com>
2023-11-03 16:03:50 -0700

    Add project files.

---
+++ TutorialProject10/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	/*
+	//Floating Point
+	float f;
+	double d;
+
+	// Integers
+	int i;
+	char c = 'd';
+
+	unsigned int ui;
+
+	ui = -4;
+	cout << (int)c << endl;
+	*/
+
+	int i = 5;
+	int j = 6;
+
+	cout << (float)i / j << endl;
+
+
+	system("PAUSE");
+	return 0;
+}
